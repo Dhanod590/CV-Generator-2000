471e6dcf8af91b96549193740eddf892cd632b8c
Dhanod590 <dhanodherath@gmail.com>
2024-08-30 21:53:24 +0530

    Add files via upload

---
+++ sam.sh
@@ -0,0 +1,70 @@
+echo Welcome to the CV Generator 20000000 made by Vasana Software!
+sleep 4
+echo Please Give your information to make a CV
+sleep 1
+echo Name of Person:
+read name
+sleep 1
+echo Age of $name:
+read age
+sleep 1
+echo Address of $name:
+read address
+sleep 1
+echo Phone Number for $name:
+read phone
+sleep 2
+echo Mother of $name
+read mom
+sleep 1
+echo Father of $name
+read dad
+sleep 1
+echo School of $name
+read edu
+sleep 1
+echo Profession of $name
+read pro
+sleep 1
+echo Experience of $name
+read xp
+sleep 1
+echo SKills of $name
+read skill
+sleep 1
+echo Email of $name
+read email
+echo SOrry, the options you are looking for are not here, so please use a diffrent tool.
+sleep 1
+echo Press enter when you are ready to start making a CV....
+read lol
+sleep 1
+clear
+
+echo ...............CV of $name....................
+echo Name:$name
+echo Age:$age
+echo Phone Number:$phone
+echo Address:$address
+echo Mother:$mom
+echo Father:$dad
+echo Professiom:$pro
+echo Expereence:$xp
+echo School:$edu
+echo SKills: $skill
+echo Email: $email
+echo ...............End of CV.....................
+echo Press enter to exit 
+read lolipop
+
+echo Press enter to view the authors of this program.
+echo If you do not want to see the authors, please press Control key with C to exit to the shell
+read lolipopcocacola
+echo You have the right to share and modify the program XD
+echo This Program was made the Fix Fusion technology.
+echo ATTENTION! This Program is still in alpha phase and more features will be implemented in newer versions of this program..
+echo I hope you liked the program.
+echo Well, thanks for reading this.
+echo Press enter to exit the program.......
+sleep 12
+
